7bfaae60d24ece5e337638eff4f1372ccc35530c
Smart Pig by Felipe <87739239+SmartPig0@users.noreply.github.com>
2022-05-07 09:24:52 -0300

    lear-more-react

---
+++ src/Calc.js
@@ -0,0 +1,16 @@
+import './hello.css';
+
+
+function Calc(){
+
+return (
+
+    <div className='container2'>
+    <h1 className='title'>Hello React Js World</h1>
+    </div>
+
+);
+
+}
+
+export default Calc;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './hello.css';
 import App from './App';
+import Calc from './Calc';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Calc />
   </React.StrictMode>
 );
+
+/*const calc = ReactDOM.createRoot(document.getElementById('calc'));
+root.render(
+  <React.StrictMode>
+    <Calc />
+  </React.StrictMode>
+);*/
